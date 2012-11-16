de1887bcda5e99a99a0dbf0da1391d7a3b08b48d
yannikmesserli <yannik.messerli@gmail.com>
2012-11-16 17:07:01 +0100

    Cosmetics for the filters, removed all double link

--- js/views/search.js
+++ js/views/search.js
@@ -67,7 +67,7 @@ define(["jquery", "radio"], function ($, radio) {
 
 		// Add info
 		var info = makeInfo(filter);
-		f.children("p").append(info);
+		f.children("p.filterItem").append(info);
 
 		// Add id
 		f.attr("id","filter" + index);
